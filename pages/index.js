4cf397a0ccc8f5964bbc7392a1904d36cd176445
0xFelipe <felipe-souza-mendes@outlook.com>
2024-11-09 18:17:53 -0300

    Instalando o Jest

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 function Home() {
-  return <h1>Em Construção 🚧</h1>;
+  return (
+    (<h1>Em Construção 🚧</h1>),
+    (
+      <img
+        alt="Em construção"
+        width="500"
+        height="300"
+        src="https://i.pinimg.com/originals/64/92/c5/6492c545bd4fa426d18f3de822ff444d.gif"
+      ></img>
+    )
+  );
 }
-<img
-  alt="Em construção"
-  width="500"
-  height="300"
-  src="https://i.pinimg.com/originals/64/92/c5/6492c545bd4fa426d18f3de822ff444d.gif"
-></img>;
 
 export default Home;
